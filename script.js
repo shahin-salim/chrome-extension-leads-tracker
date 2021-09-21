22ba0733ba0894eae9d05b47997ea15df1e6f556
shahin salim <shahinsalim82@gmail.com>
2021-09-22 01:22:57 +0530

    extension completed.deployed in chrome.

--- script.js
+++ script.js
@@ -2,39 +2,55 @@
 const inputBtn = document.getElementById("input-btn")
 let inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-myLeads = []
+const del = document.getElementById("Delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+let myLeads = []
+let oldLeds = []
 
-
-
-inputBtn.addEventListener("click", function(){
-
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-
-    console.log(typeof myLeads)
-    console.log(localStorage.getItem("myLeads"))
-
-    
+del.addEventListener("click", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
 
 })
 
 
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active :true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+     
+})
 
-function renderLeads(){
-let listItems = ""
-for(let i=0;i<myLeads.length;i++){
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
     
-    // listItems += "<li><a target='_blank' href='" +myLeads+ "'>"+myLeads[i]+"</a></li>"
-       listItems += 
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+
+
+function render(leads){
+let listItems = ""
+for(let i=0;i<leads.length;i++){
+           listItems += 
        `<li>
-       <a target='_blank' href='${myLeads[i]}'>
-       ${myLeads[i]}
+       <a target='_blank' href='${leads[i]}'>
+       ${leads[i]}
        </a>
        </li>`
-
 }
 ulEl.innerHTML= ""
 ulEl.innerHTML = listItems
